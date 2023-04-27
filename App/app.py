dfebbe2e1c20907522888b768af4e6920db0278b
Miguel Chiau <chiau.miguel@gmail.com>
2023-04-27 00:51:22 -0400

    scalling values

--- App/app.py
+++ App/app.py
@@ -13,33 +13,74 @@ def load_data():
 
 data = load_data()
 
+# To make sure all predictions values are scalled between 0 and 1
+
+
+def get_scaled_values(input_dict):
+    data = load_data()
+
+    X = data.drop(['diagnosis'], axis=1)
+
+    scaled_dict = {}
+
+    for key, value in input_dict.items():
+        max_val = X[key].max()
+        min_val = X[key].min()
+        scaled_value = (value - min_val) / (max_val - min_val)
+        scaled_dict[key] = scaled_value
+
+    return scaled_dict
+
 
 def get_radar_chart(input_data):
-    categories = ['processing cost', 'mechanical properties', 'chemical stability',
-                  'thermal stability', 'device integration']
+
+    input_data = get_scaled_values(input_data)
+
+    categories = ['Radius', 'Texture', 'Perimeter',
+                  'Area', 'Smoothness', 'Compactness',
+                  'Concavity', 'Concave Points', 'Symmetry', 'Fractal Dimension']
 
     fig = go.Figure()
 
     fig.add_trace(go.Scatterpolar(
-        r=[1, 5, 2, 2, 3],
+        r=[input_data['radius_mean'], input_data['texture_mean'], input_data['perimeter_mean'],
+           input_data['area_mean'], input_data['smoothness_mean'], input_data['compactness_mean'],
+           input_data['concavity_mean'], input_data['concave points_mean'], input_data['symmetry_mean'],
+           input_data['fractal_dimension_mean']
+           ],
         theta=categories,
         fill='toself',
         name='Product A'
     ))
     fig.add_trace(go.Scatterpolar(
-        r=[4, 3, 2.5, 1, 2],
+        r=[input_data['radius_se'], input_data['texture_se'], input_data['perimeter_se'], input_data['area_se'],
+           input_data['smoothness_se'], input_data['compactness_se'], input_data['concavity_se'],
+           input_data['concave points_se'], input_data['symmetry_se'], input_data['fractal_dimension_se']],
         theta=categories,
         fill='toself',
         name='Product B'
     ))
 
+    fig.add_trace(
+        go.Scatterpolar(
+            r=[input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
+                input_data['area_worst'], input_data['smoothness_worst'], input_data['compactness_worst'],
+                input_data['concavity_worst'], input_data['concave points_worst'], input_data['symmetry_worst'],
+                input_data['fractal_dimension_worst']],
+            theta=['Radius', 'Texture', 'Perimeter', 'Area', 'Smoothness', 'Compactness', 'Concavity', 'Concave Points',
+                   'Symmetry', 'Fractal Dimension'],
+            fill='toself',
+            name='Worst'
+        )
+    )
+
     fig.update_layout(
         polar=dict(
             radialaxis=dict(
                 visible=True,
-                range=[0, 5]
+                range=[0, 1]
             )),
-        showlegend=False
+        showlegend=True
     )
 
     return fig
@@ -59,7 +100,7 @@ def app():
     # Creating a slider for each predictor about 30 of them, since dropped 2
     with st.container():
         st.title('Breast Cancer Predictor')
-        st.write('PLease connect this app to your cytology lab to help diagnose breast cancer from your tissue sample. This app makes predictions using a Machine Learning Model whether a breast mass is benign or malignant based in the measurements it receives from your lab. You can also update the measurements manually using the sliders in the sidebar.')
+        st.write('Please connect this app to your cytology lab to help diagnose breast cancer from your tissue sample. This app makes predictions using a Machine Learning Model whether a breast mass is benign or malignant based in the measurements it receives from your lab. You can also update the measurements manually using the sliders in the sidebar.')
 
     col1, col2 = st.columns([4, 1])
     with col1:
